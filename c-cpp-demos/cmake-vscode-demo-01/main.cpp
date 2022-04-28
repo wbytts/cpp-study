c2dde935be997c8e84853c72518d0174ff8df6f5
wangbingyi <1041493283@qq.com>
2022-04-29 07:57:35 +0800

    update

---
+++ c-cpp-demos/cmake-vscode-demo-01/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int agec, char **argv)
+{
+    cout << "Hello world" << endl;
+
+    int a;
+    cout << "ÇëÊäÈëa: ";
+    cin >> a;
+    cout << "a=" << a << endl;
+
+    return 0;
+}
